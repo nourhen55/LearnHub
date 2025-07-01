1099ede7e72c1b40aaf02953244fa422706dcc7a
nourhen <henn5722@gmail.com>
2025-07-01 21:06:42 +0100

    Ignore dossier firebase

--- project-backend/firebase/firebase.config.ts
+++
@@ -1,9 +0,0 @@
-import admin from 'firebase-admin';
-import * as serviceAccount from './serviceAccountKey.json';
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
-});
-
-const db = admin.firestore();
-
-export { admin, db };
